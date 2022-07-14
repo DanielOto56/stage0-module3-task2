f369d9b3d896cc47bbb3a91cf2d84a46b6c41e71
DanielOto56 <daniel.oto.mail@gmail.com>
2022-07-14 21:10:51 +0300

    task 3, some issues to understand

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -6,11 +6,11 @@ public class AdvancedNamingConvention {
     private int age;
     private int phoneNumber;
 
-    void cAllToFriend() {
+    void callToFriend() {
         //some code
     }
 
-    void callByNumber(int Number) {
+    void callByNumber() {
         //some code
     }
 }
--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,17 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variableZero = 0;
-        int variableOne = 2000;
-        int value = 1000;
-        int variable_ = 4000;
+        int variable = 0;
+        int variableTwo = 2000;
+        int valueOne = 1000;
+        int variableThree = 4000;
         int valueTwo = 3000;
-        int variables = 5000;
-        int variablesTwo = 7000;
+        int variableFour = 5000;
+        int variableFive = 7000;
         int valueThree = 90000;
+        int valueFour = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int my_integer = 1234567;
     }
 }
